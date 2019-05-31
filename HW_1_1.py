f1c5cff5258c435339b518525b85ddd183297afe
clagunag <49294878+clagunag@users.noreply.github.com>
2019-05-30 17:33:46 -0700

    Add files via upload

---
+++ HW_1_1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+'''
+Cesar Laguna
+Python 3.6
+'''
+import numpy as np
+'''
+# 1
+Takes inputs (b, c) and provides output of A (area of a rectangle)
+Rectangle = rec
+'''
+
+b = int(input('Base (rec) :'))  #input allows for user to input their own data/ numbers
+c = int(input('Height (rec) :'))
+A_rec = b*c
+print ('Area of Rectangle', A_rec)
+
+'''
+# 1
+For area of a triangle
+'''
+
+h0 = int(input('Base (tri) :'))
+B  = int(input('Height (tri) :'))
+A_tri = 0.5*h0*B
+print ('Area of Triangle', A_tri)
